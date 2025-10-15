2478bf6ce8d2bd2338ede32cecab423d9a3f0e62
tyler.kim <tyler.kim@navercorp.com>
2025-10-15 14:32:02 +0900

    feat: logger 추가
    - logger 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.kafka:spring-kafka")
+    implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.kafka:spring-kafka-test")
